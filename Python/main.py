1a1a38a8ecb041c2f9a8f387e1d4eeefc86dc32b
j0sh21 <132760416+j0sh21@users.noreply.github.com>
2024-02-03 03:59:23 +0100

    .

--- Python/main.py
+++ Python/main.py
@@ -4,6 +4,7 @@ import config
 
 DEBUG = config.DEBUG_MODE  # Set to True to run debug_client.py instead of switch.py
 
+
 def run_app2():
     print("Start process_mock CLIENT")
     subprocess.run(["python3", "./dev/process_mock.py"])
@@ -13,27 +14,29 @@ def run_app():
     print("Start APP SERVER")
     subprocess.run(["python3", "app.py"])
 
-def run_switch_or_debug_as_subprocess():
-    if DEBUG == 1:
-        # Running debug_client.py as a subprocess when DEBUG is True
-        print("Start DEBUG CLIENT")
-        subprocess.run(["python3", r"./dev/debug_client.py"])
-    else:
-        print("Start SWITCH CLIENT")
-        # Running switch.py as a subprocess when DEBUG is False
-        subprocess.run(["python3", "switch.py"])
 
 def main():
     # Starting app.py in a separate thread
     app_thread = threading.Thread(target=run_app)
     app_thread.start()
 
+    if DEBUG == 1:
+        # Running debug_client.py as a subprocess when DEBUG is True
+        print("Start DEBUG CLIENT")
+        subprocess.run(["python3", r"./dev/debug_client.py"])
+
     if DEBUG == 2:
         app_thread2 = threading.Thread(target=run_app2())
         app_thread2.start()
 
+    if DEBUG == 3:
+        print("Start Debug process without payment and printing")
+    else:
+        print("Start SWITCH CLIENT")
+        # Running switch.py as a subprocess when DEBUG is False
+        subprocess.run(["python3", "switch.py"])
     # Running switch.py or debug_client.py as a subprocess
-    run_switch_or_debug_as_subprocess()
+
 
 if __name__ == "__main__":
     main()
