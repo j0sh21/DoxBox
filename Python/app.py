92bb40930fbaf376f69a3df88e212342e01efb15
Odemsloh <50321653+Odemsloh@users.noreply.github.com>
2024-02-03 03:22:35 +0100

    .

--- Python/app.py
+++ Python/app.py
@@ -145,8 +145,6 @@ class VendingMachineDisplay(QWidget):
         self.textLabel.move((self.width() - rect.width()) // 2, (self.height() - rect.height()) // 2)
 
     def onStateChanged(self, state):
-        global stateG
-        stateG = state
         # Mapping of states to messages
         state_messages = {
             "0": "State changed to 0",
@@ -159,19 +157,19 @@ class VendingMachineDisplay(QWidget):
         }
 
         # Update the text label based on the state
-        message = state_messages.get(stateG, "Unknown state")
-        print(str(stateG))
+        message = state_messages.get(state, "Unknown state")
+        print(str(state))
         self.textLabel.setText(message)
 
         # Add more state handling as needed
         self.movie.stop()
-        self.updateGIF(stateG)
+        self.updateGIF(state)
 
-        if stateG == "1":
+        if state == "1":
             print("State changed to 1")
             time.sleep(3)
-            stateG = "2"
-        elif stateG == "2":
+            state = "2"
+        elif state == "2":
             try:
                 if config.DEBUG_MODE > 1:
                     print("Simulate Photo")
@@ -180,17 +178,17 @@ class VendingMachineDisplay(QWidget):
                     print("img_capture.py started successfully.")
             except Exception as e:
                 print(f"Failed to start img_capture.py: {e}")
-        elif stateG == "3":
+        elif state == "3":
             time.sleep(2)
             print("take photo")
             img_capture.main()
-        elif stateG == "4":
+        elif state == "4":
             if config.DEBUG_MODE > 2:
                 print("Simulate print")
                 subprocess.run(["python3", "./dev/printer_mock.py"])
             else:
                 subprocess.run(["python3", ".print.py"])
-        elif stateG == "5":
+        elif state == "5":
             print("Tahnk You!")
 
     def updateGIF(self, state):
