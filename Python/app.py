9d125b6a0dd75511f9c8b9af58d820d857ab23aa
j0sh21 <132760416+j0sh21@users.noreply.github.com>
2024-02-06 01:44:52 +0100

    state change when gif finished

--- Python/app.py
+++ Python/app.py
@@ -48,17 +48,17 @@ class VendingMachineDisplay(QWidget):
             self.onGIFFinished()
 
     def onGIFFinished(self):
-        if self.state in("1","2","3"):
+        if self.appState.state in("1","2","3"):
             if self.state == "1":
                 appState.stateChanged.emit("2")
                 print("GIFF finished, next State: 2 and Gif")
             if self.state == "2":
                 appState.stateChanged.emit("3")
                 print("GIFF finished, next State: 3 and Gif")
-        if self.state in("4", "100"):
+        if self.appState.state in("4", "100"):
             print("GIFF finished, TODO: gif from same folder again until external state change")
             self.updateGIF(self.state) #TODO!!!
-        if self.state == 5:
+        if self.appState.state == 5:
             print("GIFF finished, initial State 0 and Gif")
             appState.stateChanged.emit("0")
 
