26bdb855e6ed57b27603e14d3fd17636c153d38e
j0sh21 <132760416+j0sh21@users.noreply.github.com>
2024-02-08 23:45:35 +0100

    tryout shutil for moving picture

--- Python/print.py
+++ Python/print.py
@@ -1,5 +1,5 @@
 import datetime
-
+import shutil
 import cups
 import os
 import config
@@ -27,6 +27,18 @@ def print_image(printer_name, image_path):
     else:
         print(f"DEBUG MODE: Simulate Print file {image_path} on {printer_name}. \nDEBUG MODE: Skip 45 sec waiting time...")
 
+def copy_file(source_path, destination_path):
+    try:
+        # Copy the source file to the destination path
+        shutil.copy(source_path, destination_path)
+        print(f"Successfully copied {source_path} to {destination_path}")
+    except FileNotFoundError:
+        print(f"Error: The file {source_path} does not exist.")
+    except PermissionError:
+        print(f"Error: Permission denied while copying {source_path}.")
+    except Exception as e:
+        print(f"An unexpected error occurred while copying {source_path}: {str(e)}")
+
 def move_image():
     pic_dir = os.path.join(config.PICTURE_SAVE_DIRECTORY, datetime.datetime.now().strftime("%Y-%m-%d"))
     print_dir = config.PRINT_DIR
@@ -34,26 +46,20 @@ def move_image():
     cwd = os.getcwd()
     os.chdir(pic_dir)
     print(f"Change directory to {pic_dir}")
+
     for filename in os.listdir():
         picture_name = filename
-    # Construct the full source and destination paths
+        # Construct the full source and destination paths
         source_picture_path = os.path.join(pic_dir, picture_name)
-        destination_picture_path = os.path.join(print_dir, picture_name)
+        destination_picture_path = print_dir
 
-    # Use os.rename() to move the picture, delete after print with os.remove
-        try:
-            os.rename(source_picture_path, destination_picture_path)
-            print(f'Successfully moved {picture_name} from {pic_dir} to {print_dir}')
-            image_path = os.path.join(print_dir, picture_name)
-            print_image(printer_name, image_path)
-            os.remove(destination_picture_path)
-            print(f'Successfully removed {picture_name} from Disk.')
-        except FileNotFoundError:
-            print(f'Error: {picture_name} not found in {pic_dir}')
-        except PermissionError:
-            print(f'Permission denied when accessing {filename}. Check file permissions.')
-        except Exception as e:
-            print(f'An error occurred while move {picture_name} to {print_dir}: {str(e)}')
+        copy_file(source_picture_path, destination_picture_path)
+        print(f'Successfully moved {picture_name} from {pic_dir} to {print_dir}')
+        image_path = os.path.join(print_dir, picture_name)
+        print_image(printer_name, image_path)
+        os.remove(destination_picture_path)
+        print(f'Successfully removed {picture_name} from Disk.')
+
     os.chdir(cwd)
     print(f"Change directory back to {cwd}")
 
