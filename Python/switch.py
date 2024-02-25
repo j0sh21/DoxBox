7747c22fef0f0f17865eab7281a86d563b28b15d
j0sh21 <132760416+j0sh21@users.noreply.github.com>
2024-02-25 04:37:45 +0100

    Update switch.py
    try error handling

--- Python/switch.py
+++ Python/switch.py
@@ -16,35 +16,44 @@ def main_loop():
     prevHash = ''
     currHash = ''
     rsJson = ''
-    response = requests.get(config.API_URL, headers={'X-API-Key': config.API_KEY})
-    prevHash = response.json()[0]['payment_hash']
-    response = None
+    try:
+        response = requests.get(config.API_URL, headers={'X-API-Key': config.API_KEY})
+        prevHash = response.json()[0]['payment_hash']
+        response = None
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching initial payment hash: {e}")
+        return
 
     while True:
-        print("Polling LNbits API for new payments")
-        response = requests.get(config.API_URL, headers={'X-API-Key': config.API_KEY})
-        if (response.status_code == 200):
-            rsJson = response.json()
-
-            currHash = rsJson[0]['payment_hash']
-
-            if (currHash != prevHash):
-                prevHash = currHash
-
-                amount = rsJson[0]['amount'] / 1000  # Amount in SATS
-
-                if (amount == config.AMOUNT_THRESHOLD):
-                    print("Sats recieved ! Sending msg = 1 to app.py!")
-                    send_message_to_app("1")
-                    time.sleep(config.POST_PAYMENT_DELAY)
+        try:
+            print("Polling LNbits API for new payments")
+            response = requests.get(config.API_URL, headers={'X-API-Key': config.API_KEY})
+            if response.status_code == 200:
+                rsJson = response.json()
+                currHash = rsJson[0]['payment_hash']
+                if currHash != prevHash:
+                    prevHash = currHash
+                    amount = rsJson[0]['amount'] / 1000
+                    if amount == config.AMOUNT_THRESHOLD:
+                        print(f"{amount} Sats received! Sending message '1' to app.py")
+                        send_message_to_app("1")
+                        time.sleep(config.POST_PAYMENT_DELAY)
+                    else:
+                        print(f"{str(amount)} Sats received, but one photo costs {config.AMOUNT_THRESHOLD}.\n Since no refund is implemented yet, pleas try again...")
+                        time.sleep(config.CHECK_INTERVAL)
+                        send_message_to_app("144")
                 else:
+                    print(f"No payment received! Fetching LNbits API again in {config.CHECK_INTERVAL} seconds...")
                     time.sleep(config.CHECK_INTERVAL)
             else:
-                time.sleep(config.CHECK_INTERVAL)
-        else:
-            exit(response.status_code)
+                print(f"Failed to poll LNbits API: {response.status_code}")
+                send_message_to_app("141")
+        except requests.exceptions.RequestException as e:
+            print(f"Error during API request: {e}")
+
+        time.sleep(config.CHECK_INTERVAL)
 
 if __name__ == '__main__':
-    print("switch.py is now running.")
+    print("Starting the main loop")
     main_thread = threading.Thread(target=main_loop)
     main_thread.start()
