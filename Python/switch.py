aa8bc46d9c7b86d1b8e2834fd2d6a9bbd38e6aa3
j0sh21 <132760416+j0sh21@users.noreply.github.com>
2024-02-25 17:49:29 +0100

    - check for network connectivity before polling LNbits API

--- Python/switch.py
+++ Python/switch.py
@@ -1,3 +1,4 @@
+import subprocess
 import config
 import requests
 import time
@@ -12,6 +13,17 @@ def send_message_to_app(message):
     except socket.error as e:
         print(f"Error in sending message to app: {e}")
 
+def check_connection():
+    command = ['ping', '-c', '1', 'google.com']
+    try:
+        # Führe den Ping-Befehl aus
+        subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+        print("Connection check successful.")
+        return True
+    except subprocess.CalledProcessError:
+        print("No Connection to the internet")
+        return False
+
 def main_loop():
     prevHash = ''
     currHash = ''
@@ -21,28 +33,40 @@ def main_loop():
     response = None
 
     while True:
-        print("Polling LNbits API for new payments")
-        response = requests.get(config.API_URL, headers={'X-API-Key': config.API_KEY})
-        if (response.status_code == 200):
-            rsJson = response.json()
+        if check_connection():
+            print("Polling LNbits API for new payments")
+            response = requests.get(config.API_URL, headers={'X-API-Key': config.API_KEY})
+            if (response.status_code == 200):
+                rsJson = response.json()
 
-            currHash = rsJson[0]['payment_hash']
+                currHash = rsJson[0]['payment_hash']
 
-            if (currHash != prevHash):
-                prevHash = currHash
+                if (currHash != prevHash):
+                    prevHash = currHash
 
-                amount = rsJson[0]['amount'] / 1000  # Amount in SATS
+                    amount = rsJson[0]['amount'] / 1000  # Amount in SATS
 
-                if (amount == config.AMOUNT_THRESHOLD):
-                    print("Sats recieved ! Sending msg = 1 to app.py!")
-                    send_message_to_app("1")
-                    time.sleep(config.POST_PAYMENT_DELAY)
+                    if (amount == config.AMOUNT_THRESHOLD):
+                        print("Sats recieved ! Sending msg = 1 to app.py!")
+                        send_message_to_app("1")
+                        time.sleep(config.POST_PAYMENT_DELAY)
+                    else:
+                        time.sleep(config.CHECK_INTERVAL)
                 else:
                     time.sleep(config.CHECK_INTERVAL)
             else:
+                print(f"LNbits API response status code: {response.status_code}")
+                print("Polling LNbits API again in 5 seconds.")
                 time.sleep(config.CHECK_INTERVAL)
         else:
-            exit(response.status_code)
+            print("No Internet Connection, checking again in 5 seconds")
+            time.sleep(config.CHECK_INTERVAL)
+            if check_connection():
+                print("Connection reestablished polling LNbits API")
+            else:
+                time.sleep(config.IMG_SLEEP_TIME_LONG)
+                print(f"Sleeped {config.IMG_SLEEP_TIME_LONG+config.CHECK_INTERVAL} seconds. Try polling again...")
+
 
 if __name__ == '__main__':
     print("switch.py is now running.")
